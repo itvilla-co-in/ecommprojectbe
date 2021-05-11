a86ea3fea9e0343e9d895c454e645498e604dd23
naren <naren.kang@gmail.com>
2021-05-12 00:29:48 +0530

    starter app for ecommerece project

--- sprint-boot-application-ecommerce/src/main/java/com/itvilla/ecomm/prod/dao/ProductRepository.java
+++ sprint-boot-application-ecommerce/src/main/java/com/itvilla/ecomm/prod/dao/ProductRepository.java
@@ -5,10 +5,19 @@ package com.itvilla.ecomm.prod.dao;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.RequestParam;
+ import org.springframework.web.bind.annotation.CrossOrigin;
+ import org.springframework.web.bind.annotation.RequestParam;
+ import com.itvilla.ecomm.prod.entity.Product;
 
+ import java.util.List;
+ import org.springframework.web.bind.annotation.RequestParam;
+
+
+@CrossOrigin(origins = "*")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategoryId(@RequestParam("id") Long id);
 
     //Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
